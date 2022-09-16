205e7b82a7d818441070db4aef4e5ecb5b39c71e
neyasbltb88 <neyasbltb_88@mail.ru>
2022-09-16 22:54:55 +0300

    Добавлены babel-плагины

--- webpack.config.js
+++ webpack.config.js
@@ -95,9 +95,15 @@ config = {
                     options: {
                         presets: ['@babel/preset-env'],
                         plugins: [
-                            '@babel/plugin-proposal-class-properties',
-                            '@babel/plugin-syntax-jsx',
-                            ['@babel/plugin-transform-react-jsx', { 'pragma': 'dom' }]
+                            ['@babel/plugin-syntax-jsx'],
+                            ['@babel/plugin-transform-react-jsx', { 'pragma': 'dom' }],
+                            ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+                            ['@babel/plugin-proposal-private-methods', { loose: true }],
+                            ['@babel/plugin-proposal-class-properties', { loose: true }],
+                            ['@babel/plugin-proposal-optional-chaining'],
+                            ['@babel/plugin-proposal-nullish-coalescing-operator'],
+                            ['@babel/plugin-proposal-logical-assignment-operators'],
+                            ['@babel/plugin-proposal-numeric-separator']
                         ]
                     }
                 }
